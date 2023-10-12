a09ca6431f5a3b2c5710e222ff31f8143a4cf95b
max-khakimzhanov <140076510+max-khakimzhanov@users.noreply.github.com>
2023-10-12 13:58:34 +0300

    Add files via upload

---
+++ DevOps1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DevOps1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Maksym Khakimzhanov!");
+        }
+    }
+}
